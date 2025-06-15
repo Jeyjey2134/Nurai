5b6a324ffdabd0a94cd4f6ccd00e444132a5d34d
Julian Köllisch <juliankoellisch@MacBook-Air-von-Julian.local>
2025-06-15 12:09:02 +0100

    fix: Optimiere Asset-Ladung und behebe Chunk-Fehler

--- next.config.js
+++ next.config.js
@@ -2,4 +2,45 @@ const withNextIntl = require('next-intl/plugin')(
   // './src/i18n.ts'
 );
 
-module.exports = withNextIntl({}); 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Optimize chunks and assets
+  webpack: (config, { isServer }) => {
+    // Optimize chunk loading
+    config.optimization.splitChunks = {
+      chunks: 'all',
+      cacheGroups: {
+        default: false,
+        vendors: false,
+        commons: {
+          name: 'commons',
+          chunks: 'all',
+          minChunks: 2,
+        },
+        shared: {
+          name: 'shared',
+          chunks: 'all',
+          minChunks: 2,
+          reuseExistingChunk: true,
+        },
+      },
+    }
+    
+    // Optimize asset loading
+    config.optimization.moduleIds = 'deterministic'
+    config.optimization.runtimeChunk = 'single'
+    
+    return config
+  },
+  // Optimize image loading
+  images: {
+    domains: ['getnurai.com'],
+    unoptimized: true,
+  },
+  // Optimize static file serving
+  poweredByHeader: false,
+  compress: true,
+}
+
+module.exports = withNextIntl(nextConfig); 
