809efd8dbc8f6d1f11c69af07e874536e2a4d4c6
Julian Köllisch <juliankoellisch@MacBook-Air-von-Julian.local>
2025-06-15 17:59:25 +0100

    fix: use standalone build instead of export

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   i18n: {
     locales: ['en', 'de', 'fr', 'es', 'it', 'tr'],
